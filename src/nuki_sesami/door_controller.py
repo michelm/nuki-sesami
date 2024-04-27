5e498df07cc6a16e4fa942c8c9210bfcb76c4e34
mmooij <michel.mooij7@gmail.com>
2024-04-27 11:25:01 +0200

    Code cleanup

--- src/nuki_sesami/door_controller.py
+++ src/nuki_sesami/door_controller.py
@@ -1,14 +1,14 @@
-#!/bin/env python3
-
-from enum import IntEnum
+import argparse
+import logging
 import os
 import sys
-import logging
+from enum import IntEnum
 from logging import Logger
 from logging.handlers import RotatingFileHandler
-import argparse
+
 import paho.mqtt.client as mqtt
 from gpiozero import Button, DigitalOutputDevice
+
 from nuki_sesami.door_state import DoorState, next_door_state
 
 
@@ -30,8 +30,8 @@ class NukiLockAction(IntEnum):
     unlock          = 1 # activate rto
     lock            = 2 # deactivate rto
     unlatch         = 3 # electric strike actuation
-    lock_and_go1    = 4 # lock ‘n’ go; activate continuous mode
-    lock_and_go2    = 5 # lock ‘n’ go with unlatch deactivate continuous mode
+    lock_and_go1    = 4 # lock&go; activate continuous mode
+    lock_and_go2    = 5 # lock&go with unlatch deactivate continuous mode
     full_lock       = 6
     fob             = 80 # (without action) fob (without action)
     button          = 90 # (without action) button (without action)
@@ -50,63 +50,65 @@ def mqtt_on_connect(client, userdata, flags, rc):
     '''
     door = userdata
     if rc == mqtt.CONNACK_ACCEPTED:
-        door.logger.info(f"(mqtt) connected; code={rc}, flags={flags}")
+        door.logger.info("(mqtt) connected; code=%i, flags=%s", rc, flags)
     else:
-        door.logger.error(f"(mqtt) connect failed; code={rc}, flags={flags}")
-    client.subscribe(f"nuki/{door._nuki_device_id}/state")
+        door.logger.error("(mqtt) connect failed; code=%i, flags=%s", rc, flags)
+    client.subscribe(f"nuki/{door.nuki_device_id}/state")
 
 
-def mqtt_on_message(client, userdata, msg):
+def mqtt_on_message(_, userdata, msg):
     '''The callback for when a PUBLISH message of Nuki smart lock state is received.
     '''
-    door = userdata    
+    door = userdata
     try:
         lock = NukiLockState(int(msg.payload))
-        door.logger.info(f"(mqtt) topic={msg.topic}, lock={lock.name}:{lock}")
+        door.logger.info("(mqtt) topic=%s, lock=%s:%i", msg.topic, lock.name, int(lock))
         door.on_lock_state_changed(lock)
-    except Exception as e:
-        door.logger.error(f"(mqtt) topic={msg.topic}, payload={msg.payload}, payload_type={type(msg.payload)}, payload_length={len(msg.payload)}, exception={e}")
-
+    except Exception:
+        door.logger.exception("(mqtt) topic=%s, payload=%s, payload_type=%s, payload_length=%i",
+            msg.topic, msg.payload, type(msg.payload), len(msg.payload)
+        )
 
 class Relay(DigitalOutputDevice):
-    def __init__(self, pin, *args, **kwargs):
-        super(Relay, self).__init__(pin, active_high=False, *args, **kwargs)
+    def __init__(self, pin, active_high):
+        super().__init__(pin, active_high=active_high)
 
 
 class PushButton(Button):
     def __init__(self, pin, userdata, *args, **kwargs):
-        super(PushButton, self).__init__(pin, *args, **kwargs)
+        super().__init__(pin, *args, **kwargs)
         self.userdata = userdata
 
 
 def pushbutton_pressed(button):
     door = button.userdata
-    door.logger.info(f"(input) door (open/hold/close) push button {button.pin} is pressed")
+    door.logger.info("(input) door (open/hold/close) push button %i is pressed", button.pin)
     door.on_pushbutton_pressed()
 
 
-class ElectricDoor():
+class ElectricDoor:
     '''Opens an electric door based on the Nuki smart lock state
 
     Subscribes as client to MQTT door status topic from 'Nuki 3.0 pro' smart lock. When the lock has been opened
     it will activate a relay, e.g. using the 'RPi Relay Board', triggering the electric door to open.
     '''
-    def __init__(self, logger: Logger, nuki_device_id: str, pushbutton_pin: int, opendoor_pin: int, openhold_mode_pin: int, openclose_mode_pin: int):
+    def __init__(self, logger: Logger, nuki_device_id: str,
+                 pushbutton_pin: int, opendoor_pin: int, openhold_mode_pin: int, openclose_mode_pin: int):
         self._logger = logger
         self._nuki_device_id = nuki_device_id
         self._nuki_state = NukiLockState.undefined
-        self._mqtt = mqtt.Client()
+        self._mqtt = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self._mqtt.on_connect = mqtt_on_connect
         self._mqtt.on_message = mqtt_on_message
         self._mqtt.user_data_set(self) # pass instance of electricdoor
         self._pushbutton = PushButton(pushbutton_pin, self)
         self._pushbutton.when_pressed = pushbutton_pressed
-        self._opendoor = Relay(opendoor_pin) # uses normally open relay (NO)
-        self._openhold_mode = Relay(openhold_mode_pin) # uses normally open relay (NO)
-        self._openclose_mode = Relay(openclose_mode_pin) # uses normally open relay (NO)
+        self._opendoor = Relay(opendoor_pin, False) # uses normally open relay (NO)
+        self._openhold_mode = Relay(openhold_mode_pin, False) # uses normally open relay (NO)
+        self._openclose_mode = Relay(openclose_mode_pin, False) # uses normally open relay (NO)
         self._state = DoorState.openclose1
 
-    def activate(self, host: str, port: int, username: str or None, password: str or None):
+    def activate(self, host: str, port: int, username: str | None, password: str | None):
         self._opendoor.off()
         self._openhold_mode.off()
         self._openclose_mode.on()
@@ -135,38 +137,43 @@ class ElectricDoor():
     def logger(self) -> Logger:
         return self._logger
 
+    @property
+    def nuki_device_id(self) -> str:
+        return self._nuki_device_id
+
     def mode(self, openhold: bool):
         if openhold:
-            self.logger.info(f"(mode) open and hold")
+            self.logger.info("(mode) open and hold")
             self._openhold_mode.on()
             self._openclose_mode.off()
         else:
-            self.logger.info(f"(mode) open/close")
+            self.logger.info("(mode) open/close")
             self._openhold_mode.off()
             self._openclose_mode.on()
 
     def lock_action(self, action: NukiLockAction):
-        self.logger.info(f"(lock) request action={action.name}:{action}")
+        self.logger.info("(lock) request action=%s:%i", action.name, int(action))
         self._mqtt.publish(f"nuki/{self._nuki_device_id}/lockAction", int(action))
 
     def open(self):
-        self.logger.info(f"(open) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(open) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
         if self.lock not in [NukiLockState.unlatched, NukiLockState.unlatching]:
             self.lock_action(NukiLockAction.unlatch)
 
     def close(self):
-        self.logger.info(f"(close) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(close) state=%s:%i, lock=%s:%i", self.state.name, self.state, self.lock.name, self.lock)
         if self.lock not in [NukiLockState.unlatched, NukiLockState.unlatching, NukiLockState.unlocked]:
             self.lock_action(NukiLockAction.unlock)
         self.mode(openhold=False)
 
     def on_lock_state_changed(self, lock: NukiLockState):
-        self.logger.info(f"(lock_state_changed) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock} -> {lock.name}:{lock}")
+        self.logger.info("(lock_state_changed) state=%s:%i, lock=%s:%i -> %s:%i",
+                         self.state.name, self.state, self.lock.name, self.lock, lock.name, lock)
         if self.lock == NukiLockState.unlatching and lock == NukiLockState.unlatched:
             if self.openhold:
                 self.mode(openhold=True)
             else:
-                self.logger.info(f"(relay) opening door")
+                self.logger.info("(relay) opening door")
                 self._opendoor.blink(on_time=1, off_time=1, n=1, background=True)
         elif lock not in [NukiLockState.unlatched, NukiLockState.unlatching] and self.state == DoorState.openclose2:
             self._state = DoorState.openclose1
@@ -174,7 +181,8 @@ class ElectricDoor():
 
     def on_pushbutton_pressed(self):
         self._state = next_door_state(self._state)
-        self.logger.info(f"(pushbutton_pressed) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(pushbutton_pressed) state=%s:%i, lock=%s:%i",
+                         self.state.name, self.state, self.lock.name, self.lock)
         if self.state == DoorState.openclose1:
             self.close()
         elif self.state == DoorState.openclose2:
@@ -185,15 +193,16 @@ class ElectricDoor():
 
 class ElectricDoorPushbuttonOpenHold(ElectricDoor):
     '''Electric door with pushbutton 'open and hold' logic
-    
+
     When pressing the pushbutton the door will be opened and held open until the pushbutton is pressed again.
     '''
     def __init__(self, *arg, **kwargs):
-        super(ElectricDoorPushbuttonOpenHold, self).__init__(*arg, **kwargs)
+        super().__init__(*arg, **kwargs)
 
     def on_pushbutton_pressed(self):
         self._state = DoorState.openhold if self.state == DoorState.openclose1 else DoorState.openclose1
-        self.logger.info(f"(pushbutton_pressed) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(pushbutton_pressed) state=%s:%i, lock=%s:%i",
+                         self.state.name, self.state, self.lock.name, self.lock)
         if self.state == DoorState.openhold:
             self.open()
         else:
@@ -202,29 +211,32 @@ class ElectricDoorPushbuttonOpenHold(ElectricDoor):
 
 class ElectricDoorPushbuttonOpen(ElectricDoor):
     '''Electric door with pushbutton open logic
-    
+
     When pressing the pushbutton the door will be opened for a few seconds after which it will be closed again.
     '''
     def __init__(self, *arg, **kwargs):
-        super(ElectricDoorPushbuttonOpen, self).__init__(*arg, **kwargs)
+        super().__init__(*arg, **kwargs)
 
     def on_pushbutton_pressed(self):
-        self.logger.info(f"(pushbutton_pressed) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(pushbutton_pressed) state=%s:%i, lock=%s:%i",
+                         self.state.name, self.state, self.lock.name, self.lock)
         self.open()
 
 
 class ElectricDoorPushbuttonToggle(ElectricDoor):
     '''Electric door with pushbutton toggle logic
 
-    When pressing the pushbutton the door will open, if during the smart lock unlatching phase of the pushbutton is pressed again
-    the door will be held open until the pushbutton is pressed again.
+    When pressing the pushbutton the door will open, if during the smart lock unlatching
+    phase of the pushbutton is pressed again the door will be held open until the pushbutton
+    is pressed again.
     '''
     def __init__(self, *arg, **kwargs):
-        super(ElectricDoorPushbuttonToggle, self).__init__(*arg, **kwargs)
+        super().__init__(*arg, **kwargs)
 
     def on_pushbutton_pressed(self):
         self._state = next_door_state(self._state)
-        self.logger.info(f"(pushbutton_pressed) state={self.state.name}:{self.state}, lock={self.lock.name}:{self.lock}")
+        self.logger.info("(pushbutton_pressed) state=%s:%s, lock=%s:%s",
+                         self.state.name, self.state, self.lock.name, self.lock)
         if self.state == DoorState.openclose1:
             self.close()
         elif self.state == DoorState.openclose2:
@@ -235,8 +247,8 @@ class ElectricDoorPushbuttonToggle(ElectricDoor):
 
 def getlogger(name: str, path: str, level: int = logging.INFO) -> Logger:
     '''Returns a logger instance for the given name and path.
-    
-    The logger for will rotating log files with a maximum size of 1MB and 
+
+    The logger for will rotating log files with a maximum size of 1MB and
     a maximum of 10 log files.
 
     Parameters:
@@ -271,7 +283,8 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument('device', help="nuki hexadecimal device id, e.g. 3807B7EC", type=str)
-    parser.add_argument('-H', '--host', help="hostname or IP address of the mqtt broker, e.g. 'mqtt.local'", default='localhost', type=str)
+    parser.add_argument('-H', '--host',
+        help="hostname or IP address of the mqtt broker, e.g. 'mqtt.local'", default='localhost', type=str)
     parser.add_argument('-p', '--port', help="mqtt broker port number", default=1883, type=int)
     parser.add_argument('-U', '--username', help="mqtt authentication username", default=None, type=str)
     parser.add_argument('-P', '--password', help="mqtt authentication secret", default=None, type=str)
@@ -279,51 +292,50 @@ def main():
     parser.add_argument('-2', '--opendoor', help="door open relay (gpio)pin", default=26, type=int)
     parser.add_argument('-3', '--openhold_mode', help="door open and hold mode relay (gpio)pin", default=20, type=int)
     parser.add_argument('-4', '--openclose_mode', help="door open/close mode relay (gpio)pin", default=21, type=int)
-    parser.add_argument('-B', '--buttonlogic', help="pushbutton logic when pressed; 0=openhold,1=open,2=toggle", default=0, type=int)
+    parser.add_argument('-B', '--buttonlogic', help="pushbutton logic when pressed; 0=openhold,1=open,2=toggle",
+                        default=0, type=int)
     parser.add_argument('-V', '--verbose', help="be verbose", action='store_true')
 
     args = parser.parse_args()
-
-    if is_virtual_env():    
-        logpath = os.path.join(sys.prefix, 'var/log/nuki-sesami')
-    else:
-        logpath = '/var/log/nuki-sesami'
+    logpath = os.path.join(sys.prefix, 'var/log/nuki-sesami') if is_virtual_env() else '/var/log/nuki-sesami'
 
     if not os.path.exists(logpath):
-        print(f"mkdir -p {logpath}")
         os.makedirs(logpath)
 
     logger = getlogger('nuki-sesami', logpath, level=logging.DEBUG if args.verbose else logging.INFO)
-    logger.debug(f"args.device={args.device}")
-    logger.debug(f"args.host={args.host}")
-    logger.debug(f"args.port={args.port}")
-    logger.debug(f"args.username={args.username}")
-    logger.debug(f"args.password=***")
-    logger.debug(f"args.pushbutton=${args.pushbutton}")
-    logger.debug(f"args.opendoor=${args.opendoor}")
-    logger.debug(f"args.openhold_mode=${args.openhold_mode}")
-    logger.debug(f"args.openclose_mode=${args.openclose_mode}")
-    logger.debug(f"args.buttonlogic=${args.buttonlogic}")
+    logger.debug("args.device=%s", args.device)
+    logger.debug("args.host=%s", args.host)
+    logger.debug("args.port=%s", args.port)
+    logger.debug("args.username=%s", args.username)
+    logger.debug("args.password=***")
+    logger.debug("args.pushbutton=%s", args.pushbutton)
+    logger.debug("args.opendoor=%s", args.opendoor)
+    logger.debug("args.openhold_mode=%s", args.openhold_mode)
+    logger.debug("args.openclose_mode=%s", args.openclose_mode)
+    logger.debug("args.buttonlogic=%s", args.buttonlogic)
 
     try:
         buttonlogic = PushbuttonLogic(args.buttonlogic)
     except ValueError:
-        logger.error(f"invalid (push)button logic; --buttonlogic={args.buttonlogic}")
+        logger.exception("invalid (push)button logic; --buttonlogic=%r", args.buttonlogic)
         sys.exit(1)
 
     if buttonlogic == PushbuttonLogic.open:
-        door = ElectricDoorPushbuttonOpen(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode, args.openclose_mode)
+        door = ElectricDoorPushbuttonOpen(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode,
+                                          args.openclose_mode)
     elif buttonlogic == PushbuttonLogic.toggle:
-        door = ElectricDoorPushbuttonToggle(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode, args.openclose_mode)
+        door = ElectricDoorPushbuttonToggle(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode,
+                                            args.openclose_mode)
     else:
-        door = ElectricDoorPushbuttonOpenHold(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode, args.openclose_mode)
+        door = ElectricDoorPushbuttonOpenHold(logger, args.device, args.pushbutton, args.opendoor, args.openhold_mode,
+                                              args.openclose_mode)
 
     try:
         door.activate(args.host, args.port, args.username, args.password)
     except KeyboardInterrupt:
         logger.info("program terminated; keyboard interrupt")
-    except Exception as e:
-        logger.error(f"something went wrong, exception; {e}")
+    except Exception:
+        logger.exception("something went wrong, exception")
 
 
 if __name__ == "__main__":
--- src/nuki_sesami/sesami_systemd.py
+++ src/nuki_sesami/sesami_systemd.py
@@ -1,13 +1,13 @@
-#!/bin/env python3
-
-import os
-import sys
 import argparse
-import subprocess
+import logging
+import os
 import shutil
+import subprocess
+import sys
+from logging import Logger
+from logging.handlers import RotatingFileHandler
 
-
-SYSTEMD_TEMPLATE = f'''[Unit]
+SYSTEMD_TEMPLATE = '''[Unit]
 Description=Electric door controller using a Nuki 3.0 pro smart lock
 After=network.target
 Wants=Network.target
@@ -27,72 +27,112 @@ WantedBy=multi-user.target
 '''
 
 
-def nuki_sesami_systemd(device: str, host: str, username: str, password: str, remove: bool = False)  -> None:
-    systemd_fname = f'/lib/systemd/system/nuki-sesami.service'
+def nuki_sesami_systemd(logger: Logger, device: str, host: str, username: str, password: str,
+                        remove: bool = False)  -> None:
+    systemd_fname = '/lib/systemd/system/nuki-sesami.service'
 
     if remove:
-        subprocess.run(["systemctl", "stop", "nuki-sesami"])
-        subprocess.run(["systemctl", "disable", "nuki-sesami"])
-        subprocess.run(["rm", "-vrf", systemd_fname])
+        subprocess.run(["/usr/bin/systemctl", "stop", "nuki-sesami"], check=False)
+        subprocess.run(["/usr/bin/systemctl", "disable", "nuki-sesami"], check=False)
+        subprocess.run(["/usr/bin/rm", "-vrf", systemd_fname], check=False)
         return
 
-    bin = shutil.which('nuki-sesami')
-    if not bin:
-        print(f"Failed to detect 'nuki-sesami' binary")
+    sesami = shutil.which('nuki-sesami')
+    if not sesami:
+        logger.error("Failed to detect 'nuki-sesami' binary")
         sys.exit(1)
 
-    try:
-        env = 'PYTHONPATH=%s:$PYTHONPATH' % [x for x in sys.path if x.startswith('/home/')][0]
-    except:
-        env = ''
+    pth = [x for x in sys.path if x.startswith('/home/')]
+    env = 'PYTHONPATH=%s:$PYTHONPATH' % pth[0] if len(pth) else ''
 
     with open(systemd_fname, 'w+') as f:
-        f.write(SYSTEMD_TEMPLATE % (env, bin, device, host, username, password))
-        print(f"Created systemd file; '{systemd_fname}'")
+        f.write(SYSTEMD_TEMPLATE % (env, sesami, device, host, username, password))
+        logger.info("Created systemd file; '%s'", systemd_fname)
 
     try:
-        subprocess.run(["systemctl", "daemon-reload"], check=True)
-        subprocess.run(["systemctl", "enable", "nuki-sesami"], check=True)
-        subprocess.run(["systemctl", "start", "nuki-sesami"], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Something went wrong: {e}")
+        subprocess.run(["/usr/bin/systemctl", "dAaemon-reload"], check=True)
+        subprocess.run(["/usr/bin/systemctl", "enable", "nuki-sesami"], check=True)
+        subprocess.run(["/usr/bin/systemctl", "start", "nuki-sesami"], check=True)
+    except subprocess.CalledProcessError:
+        logger.exception("Failed to install nuki-sesami systemd service")
         sys.exit(1)
+
+
+def getlogger(name: str, path: str, level: int = logging.INFO) -> Logger:
+    '''Returns a logger instance for the given name and path.
+
+    The logger for will rotating log files with a maximum size of 1MB and
+    a maximum of 10 log files.
+
+    Parameters:
+    * name: name of the logger, e.g. 'nuki-sesami'
+    * path: complete path for storing the log files, e.g. '/var/log/nuki-sesami'
+    * level: logging level, e.g; logging.DEBUG
+
+    '''
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+    logger.addHandler(handler)
+    handler = RotatingFileHandler(f'{os.path.join(path,name)}.log', maxBytes=1048576, backupCount=10)
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+    logger.addHandler(handler)
+    return logger
+
+
+def is_virtual_env():
+    '''Returns true when running in a virtual environment.'''
+    return sys.prefix != sys.base_prefix
 
 
 def main():
     parser = argparse.ArgumentParser(
         prog='nuki-sesami-systemd',
         description='Setup nuki-sesami as systemd service',
-        epilog='The way is shut. It was made by those who are Dead, and the Dead keep it, until the time comes. The way is shut.',
+        epilog='''The way is shut.
+        It was made by those who are Dead, and the Dead keep it, until the time comes.
+        The way is shut.''',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('device', help="nuki hexadecimal device id, e.g. 3807B7EC", type=str)
-    parser.add_argument('-H', '--host', help="hostname or IP address of the mqtt broker, e.g. 'mqtt.local'", default='localhost', type=str)
+    parser.add_argument('device',
+                        help="nuki hexadecimal device id, e.g. 3807B7EC", type=str)
+    parser.add_argument('-H', '--host', help="hostname or IP address of the mqtt broker, e.g. 'mqtt.local'",
+                        default='localhost', type=str)
     parser.add_argument('-U', '--username', help="mqtt authentication username", default=None, type=str)
     parser.add_argument('-P', '--password', help="mqtt authentication secret", default=None, type=str)
     parser.add_argument('-V', '--verbose', help="be verbose", action='store_true')
     parser.add_argument('-R', '--remove', help="Remove nuki-sesami systemd service", action='store_true')
 
     args = parser.parse_args()
+    root = sys.prefix if is_virtual_env() else '/'
+    logpath = os.path.join(root, 'var/log/nuki-sesami-daemon')
+
+    if not os.path.exists(logpath):
+        os.makedirs(logpath)
+
+    logger = getlogger('nuki-sesami', logpath, level=logging.DEBUG if args.verbose else logging.INFO)
 
     if args.verbose:
-        print(f"device      : {args.device}")
-        print(f"host        : {args.host}")
-        print(f"username    : {args.username}")
-        print(f"password    : ***")
-        print(f"remove      : {args.remove}")
+        logger.debug("device      : %s", args.device)
+        logger.debug("host        : %s", args.host)
+        logger.debug("username    : %s", args.username)
+        logger.debug("password    : ***")
+        logger.debug("remove      : %s", args.remove)
 
     if 'VIRTUAL_ENV' in os.environ:
-        print("Virtual environment detected, systemd is not supported")
+        logger.error("Virtual environment detected, systemd is not supported")
         sys.exit(1)
 
     try:
-        nuki_sesami_systemd(args.device, args.host, args.username, args.password, args.remove)
+        nuki_sesami_systemd(logger, args.device, args.host, args.username, args.password, args.remove)
     except KeyboardInterrupt:
-        print("Program terminated")
-    except Exception as e:
-        print(f"Something went wrong: {e}")
+        logger.info("Program terminated")
+    except Exception:
+        logger.exception("System daemon installation failed")
 
 
 if __name__ == "__main__":
